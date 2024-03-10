8f7555de4f7a4e104b55b9faae608e389b39e8f4
Angela Hempkmeier <aggsdpz@gmail.com>
2024-03-10 15:51:39 -0300

    feat(PlanetController): implementation of findAll method, using Example importation

--- src/main/java/com/example/swplanetapi/repositories/PlanetRepository.java
+++ src/main/java/com/example/swplanetapi/repositories/PlanetRepository.java
@@ -1,11 +1,16 @@
 package com.example.swplanetapi.repositories;
 
 import com.example.swplanetapi.entities.Planet;
+import org.springframework.data.domain.Example;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.QueryByExampleExecutor;
 
+import java.util.List;
 import java.util.Optional;
 
-public interface PlanetRepository extends CrudRepository<Planet, Long> {
+public interface PlanetRepository extends CrudRepository<Planet, Long>, QueryByExampleExecutor<Planet> {
 
     Optional<Planet> findByName(String name);
+    @Override
+    <S extends Planet> List<S> findAll(Example<S> example);
 }
