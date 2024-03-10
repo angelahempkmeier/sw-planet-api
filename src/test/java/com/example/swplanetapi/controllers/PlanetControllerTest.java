ba112bd66b911573fdb4f9bf82d770519c35f6fa
Angela Hempkmeier <aggsdpz@gmail.com>
2024-03-10 18:42:26 -0300

    feat(PlanetControllerTest): implementation of test for create planet with invalid data

--- src/test/java/com/example/swplanetapi/controllers/PlanetControllerTest.java
+++ src/test/java/com/example/swplanetapi/controllers/PlanetControllerTest.java
@@ -1,17 +1,21 @@
 package com.example.swplanetapi.controllers;
 
 import static com.example.swplanetapi.common.PlanetConstants.PLANET;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
+import com.example.swplanetapi.entities.Planet;
 import com.example.swplanetapi.services.PlanetService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -32,4 +36,26 @@ public class PlanetControllerTest {
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$").value(PLANET));
     }
+
+    @Test
+    public void createPlanet_WithInvalidData_ReturnsBadRequest() throws Exception {
+        Planet emptyPlanet = new Planet();
+        Planet invalidPlanet = new Planet("", "", "");
+
+        mockMvc.perform(post("/planets").content(objectMapper.writeValueAsString(emptyPlanet)).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isUnprocessableEntity());
+
+        mockMvc.perform(post("/planets").content(objectMapper.writeValueAsString(invalidPlanet)).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isUnprocessableEntity());
+
+    }
+
+    @Test
+    public void createPlanet_WithExistingName_ReturnsConflict() throws Exception {
+        when(planetService.create(any())).thenThrow(DataIntegrityViolationException.class);
+
+        mockMvc.perform(post("/planets").content(objectMapper.writeValueAsString(PLANET)).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isConflict());
+    }
+
 }
