58c806afb15ed6f39ec89e6fd3c66cf51a674337
Angela Hempkmeier <aggsdpz@gmail.com>
2024-03-10 15:10:02 -0300

    refact(PlanetConstants): another static constant for common directory

--- src/test/java/com/example/swplanetapi/common/PlanetConstants.java
+++ src/test/java/com/example/swplanetapi/common/PlanetConstants.java
@@ -4,4 +4,8 @@ import com.example.swplanetapi.entities.Planet;
 
 public class PlanetConstants {
     public static final Planet PLANET = new Planet("name", "climate", "terrain");
+    public static final Planet INVALID_PLANET = new Planet("", "", "");
+    public static final Long VALID_ID = 1L;
+    public static final Long INVALID_ID = 103L;
+
 }
