4e3df1580c5118c364fee15fa153a750242e05a6
Angela Hempkmeier <aggsdpz@gmail.com>
2024-03-10 16:44:44 -0300

    feat(PlanetRepositoryTest): implementation for test of creation of planet with invalid data

--- src/test/java/com/example/swplanetapi/repositories/PlanetRepositoryTest.java
+++ src/test/java/com/example/swplanetapi/repositories/PlanetRepositoryTest.java
@@ -3,6 +3,8 @@ package com.example.swplanetapi.repositories;
 import com.example.swplanetapi.entities.Planet;
 
 import static org.assertj.core.api.Assertions.*;
+
+import org.h2.table.Plan;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -28,6 +30,15 @@ public class PlanetRepositoryTest {
         assertThat(sut.getName()).isEqualTo(PLANET.getName());
         assertThat(sut.getClimate()).isEqualTo(PLANET.getClimate());
         assertThat(sut.getTerrain()).isEqualTo(PLANET.getTerrain());
+    }
+
+    @Test
+    public void createPlanet_WithInvalidData_ThrowsException(){
+        Planet emptyPlanet = new Planet();
+        Planet invalidPlanet = new Planet("", "", "");
+
+        assertThatThrownBy(() -> planetRepository.save(emptyPlanet)).isInstanceOf(RuntimeException.class);
+        assertThatThrownBy(() -> planetRepository.save(invalidPlanet)).isInstanceOf(RuntimeException.class);
     }
 
 
