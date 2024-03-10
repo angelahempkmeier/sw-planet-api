c443a7c350b80f6a1c893ec2d3ca000c6c52bb5f
Angela Hempkmeier <aggsdpz@gmail.com>
2024-03-10 17:19:54 -0300

    feat(PlanetControllerTest): testing controller layer, creating planet with valid data

---
+++ src/test/java/com/example/swplanetapi/controllers/PlanetControllerTest.java
@@ -0,0 +1,35 @@
+package com.example.swplanetapi.controllers;
+
+import static com.example.swplanetapi.common.PlanetConstants.PLANET;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+import com.example.swplanetapi.services.PlanetService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(PlanetController.class)
+public class PlanetControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private PlanetService planetService;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    public void createPlanet_WithValidData_ReturnsCreated() throws Exception {
+        when(planetService.create(PLANET)).thenReturn(PLANET);
+
+        mockMvc.perform(post("/planets").content(objectMapper.writeValueAsString(PLANET)).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$").value(PLANET));
+    }
+}
