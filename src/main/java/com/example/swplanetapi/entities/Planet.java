a273afa1f851513bd53ed37790562e6dbf2483d1
Angela Hempkmeier <aggsdpz@gmail.com>
2024-03-10 15:50:51 -0300

    feat(Planet): creating constructor for the filter feature

--- src/main/java/com/example/swplanetapi/entities/Planet.java
+++ src/main/java/com/example/swplanetapi/entities/Planet.java
@@ -16,6 +16,11 @@ public class Planet {
     public Planet() {
     }
 
+    public Planet(String climate, String terrain){
+        this.climate = climate;
+        this.terrain = terrain;
+    }
+
     public Planet(String name, String climate, String terrain) {
         this.name = name;
         this.climate = climate;
