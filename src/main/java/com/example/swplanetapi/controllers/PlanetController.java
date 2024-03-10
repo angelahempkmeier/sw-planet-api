f827f850ee825f24a69bc1da4a747224b4eb2ede
Angela Hempkmeier <aggsdpz@gmail.com>
2024-03-10 15:10:41 -0300

    feat(PlanetController): get by id implementation

--- src/main/java/com/example/swplanetapi/controllers/PlanetController.java
+++ src/main/java/com/example/swplanetapi/controllers/PlanetController.java
@@ -5,10 +5,7 @@ import com.example.swplanetapi.services.PlanetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/planets")
@@ -19,5 +16,11 @@ public class PlanetController {
     @PostMapping
     public ResponseEntity<Planet> create(@RequestBody Planet planet){
         return ResponseEntity.status(HttpStatus.CREATED).body(service.create(planet));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Planet> get(@PathVariable("id") Long id){
+        return service.get(id).map(planet -> ResponseEntity.ok(planet))
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
