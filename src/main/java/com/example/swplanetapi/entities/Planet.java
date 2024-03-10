90ef433529fa20b2608793a8183b5fd8d4e07300
Angela Hempkmeier <aggsdpz@gmail.com>
2024-03-10 17:02:00 -0300

    fix(Planet): adding the unique param in the column annotation for the name attribute

--- src/main/java/com/example/swplanetapi/entities/Planet.java
+++ src/main/java/com/example/swplanetapi/entities/Planet.java
@@ -11,7 +11,7 @@ public class Planet {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @NotEmpty
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String name;
     @NotEmpty
     @Column(nullable = false)
