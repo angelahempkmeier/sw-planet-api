66a4bab2df369fd67df54f5541d522941c6338eb
Angela Hempkmeier <aggsdpz@gmail.com>
2024-03-10 14:14:54 -0300

    feat(Planet): created Planet entity

---
+++ src/main/java/com/example/swplanetapi/entities/Planet.java
@@ -0,0 +1,61 @@
+package com.example.swplanetapi.entities;
+
+import jakarta.persistence.*;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+
+@Entity
+@Table(name = "planets")
+public class Planet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String climate;
+    private String terrain;
+
+    public Planet() {
+    }
+
+    public Planet(String name, String climate, String terrain) {
+        this.name = name;
+        this.climate = climate;
+        this.terrain = terrain;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getClimate() {
+        return climate;
+    }
+
+    public void setClimate(String climate) {
+        this.climate = climate;
+    }
+
+    public String getTerrain() {
+        return terrain;
+    }
+
+    public void setTerrain(String terrain) {
+        this.terrain = terrain;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return EqualsBuilder.reflectionEquals(obj, this);
+    }
+}
