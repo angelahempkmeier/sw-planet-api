81a719150c06147788512ae6e3de8332e55f4519
Angela Hempkmeier <aggsdpz@gmail.com>
2024-03-10 15:18:27 -0300

    feat(PlanetRepository): signature of the getbyname method

--- src/main/java/com/example/swplanetapi/repositories/PlanetRepository.java
+++ src/main/java/com/example/swplanetapi/repositories/PlanetRepository.java
@@ -2,6 +2,10 @@ package com.example.swplanetapi.repositories;
 
 import com.example.swplanetapi.entities.Planet;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
 public interface PlanetRepository extends CrudRepository<Planet, Long> {
 
+    Optional<Planet> findByName(String name);
 }
