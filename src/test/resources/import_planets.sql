73f4cc9efda77091f6468609a715edd3b239464b
Angela Hempkmeier <aggsdpz@gmail.com>
2024-03-12 19:34:08 -0300

    resource(import_planets): query for adding a list of planets

---
+++ src/test/resources/import_planets.sql
@@ -0,0 +1,3 @@
+INSERT INTO planets (id, name, climate, terrain) values (1, 'TATOOINE', 'ARID', 'DESERT');
+INSERT INTO planets (id, name, climate, terrain) values (2, 'Alderaan', 'temperate', 'grasslands, mountains');
+INSERT INTO planets (id, name, climate, terrain) values (3, 'Yavin IV', 'temperate, tropical', 'jungle, rainforest');
