27efb1346728f8389b50ea628c940534d36f97a0
Angela Hempkmeier <aggsdpz@gmail.com>
2024-03-12 19:32:11 -0300

    feat(PlanetConstants): implementation of list of planets

--- src/test/java/com/example/swplanetapi/common/PlanetConstants.java
+++ src/test/java/com/example/swplanetapi/common/PlanetConstants.java
@@ -2,10 +2,26 @@ package com.example.swplanetapi.common;
 
 import com.example.swplanetapi.entities.Planet;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlanetConstants {
     public static final Planet PLANET = new Planet("name", "climate", "terrain");
     public static final Planet INVALID_PLANET = new Planet("", "", "");
     public static final Long VALID_ID = 1L;
     public static final Long INVALID_ID = 103L;
+
+    public static final Planet TATOOINE = new Planet(1L, "Tatooine", "arid", "desert");
+    public static final Planet ALDERAAN = new Planet(2L, "Alderaan", "temperate", "grasslands, mountains");
+    public static final Planet YAVINIV = new Planet(3L, "Yavin IV", "temperate, tropical", "jungle, rainforest");
+
+    public static final List<Planet> PLANETS = new ArrayList<>(){
+        {
+            add(TATOOINE);
+            add(ALDERAAN);
+            add(YAVINIV);
+        }
+    };
+
 
 }
