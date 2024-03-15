22e8021aa574887e67f509f1024ec2a26eec3f65
Angela Hempkmeier <aggsdpz@gmail.com>
2024-03-15 19:06:21 -0300

    feat(PlanetIT): test methods for remove, list with filters, get by name, get all

--- src/test/java/com/example/swplanetapi/PlanetIT.java
+++ src/test/java/com/example/swplanetapi/PlanetIT.java
@@ -1,18 +1,21 @@
 package com.example.swplanetapi;
 
-import static com.example.swplanetapi.common.PlanetConstants.PLANET;
-import static com.example.swplanetapi.common.PlanetConstants.TATOOINE;
+import static com.example.swplanetapi.common.PlanetConstants.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.example.swplanetapi.entities.Planet;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
+
+import java.util.Arrays;
 
 @ActiveProfiles("it")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -39,10 +42,52 @@ public class PlanetIT {
 
         assertThat(sut.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(sut.getBody()).isEqualTo(TATOOINE);
-
-
     }
 
+    @Test
+    public void getPlanetByName_ReturnsPlanet(){
+        ResponseEntity<Planet> sut = restTemplate.getForEntity("/planets/name/Alderaan", Planet.class);
+
+        assertThat(sut.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(sut.getBody().getName()).isEqualTo(ALDERAAN.getName());
+    }
+
+    @Test
+    public void listPlanets_ReturnsAllPlanets(){
+        ResponseEntity<Planet[]> sut = restTemplate.getForEntity("/planets", Planet[].class);
+
+        assertThat(sut.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(sut.getBody()).isNotEmpty();
+        assertThat(Arrays.stream(sut.getBody()).toList()).hasSize(3);
+        assertThat(sut.getBody()[0]).isEqualTo(TATOOINE);
+    }
+
+    @Test
+    public void listPlanets_ByClimate_ReturnsPlanets(){
+        ResponseEntity<Planet[]> sut = restTemplate.getForEntity("/planets?climate=" + TATOOINE.getClimate(), Planet[].class);
+
+        assertThat(sut.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(sut.getBody()).isNotEmpty();
+        assertThat(Arrays.stream(sut.getBody()).toList()).hasSize(1);
+        assertThat(sut.getBody()[0]).isEqualTo(TATOOINE);
+    }
+
+    @Test
+    public void listPlanets_ByTerrain_ReturnsPlanets(){
+        ResponseEntity<Planet[]> sut = restTemplate.getForEntity("/planets?terrain=" + TATOOINE.getTerrain(), Planet[].class);
+
+        assertThat(sut.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(sut.getBody()).isNotEmpty();
+        assertThat(Arrays.stream(sut.getBody()).toList()).hasSize(1);
+        assertThat(sut.getBody()[0]).isEqualTo(TATOOINE);
+    }
+
+    @Test
+    public void removePlanet_ReturnsNoContent(){
+        ResponseEntity<Void> sut = restTemplate.exchange("/planets/" + TATOOINE.getId(), HttpMethod.DELETE, null, Void.class);
+
+        assertThat(sut.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+    }
 
 
 
